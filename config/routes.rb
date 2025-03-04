54dc9b01fc388cffe67ff91b3b96b3b9173684ea
user <user@email.com>
2025-03-04 21:00:25 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, 
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' },
+    skip: [:sessions, :registrations, :passwords]
+    
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,4 +22,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resource :profile, only: [:show]
+
+  # Set root to the GitHub sign in button
+  root 'pages#home'
 end
